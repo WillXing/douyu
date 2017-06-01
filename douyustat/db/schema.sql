cf8ee2a0ca0e833f13a65ac37fb5a4be2fd60cd2
Xing <yellowstar1992@hotmail.com>
2017-06-01 23:39:14 +0800

    db operation

---
+++ douyustat/db/schema.sql
@@ -0,0 +1,9 @@
+use spider;
+
+create table live (
+  date DATE,
+  hour INT(10),
+  name VARCHAR(255),
+  audience_total INT(32),
+  live_num_total INT(32)
+);
--- douyustat/spiders/main.py
+++ douyustat/spiders/main.py
@@ -1,9 +1,10 @@
 # -- coding: UTF-8
+import datetime
 import logging
 import re
 import time
-import MySQLdb as mdb
 
+import MySQLdb as mdb
 import scrapy
 from scrapy.shell import inspect_response
 
@@ -14,6 +15,9 @@ class DouYuSpider(scrapy.Spider):
   current=1
   totalPage=90
   result = {}
+
+  DB_NAME = "spider"
+  TABLE_NAME = "live"
 
   def start_requests(self):
     self.logger.log(logging.DEBUG, "****************** START ******************")
@@ -43,14 +47,28 @@ class DouYuSpider(scrapy.Spider):
         self.result[category] = {"audience_total": audience_num, "live_num_total": 1}
 
     if self.current >= self.totalPage:
+      conn = mdb.connect("localhost", "root", "")
+      conn.select_db(self.DB_NAME)
+      cursor = conn.cursor()
+      
+      date = datetime.datetime.now()
+      
+      insert_set = []
       for key in self.result.keys():
-        each = {
-          "name": key, 
-          "audience_total": self.result[key]["audience_total"],
-          "live_num_total": self.result[key]["live_num_total"]
-        }
-        yield each
+        audience_total = self.result[key]["audience_total"]
+        live_num_total = self.result[key]["live_num_total"]
+        
+        date_to_insert = date.strftime("%Y-%m-%d")
+        hour_to_insert = date.strftime("%H")
+        record_existing = cursor.execute("SELECT * FROM "+ self.TABLE_NAME +" WHERE name=%s AND date=%s AND hour=%s LIMIT 1", (key, date_to_insert, hour_to_insert)) > 0
+        if record_existing:
+          cursor.execute("UPDATE "+ self.TABLE_NAME +" SET audience_total=%s, live_num_total=%s WHERE name=%s AND date=%s AND hour=%s", (audience_total, live_num_total, key, date_to_insert, hour_to_insert))
+        else:
+          cursor.execute("INSERT INTO "+ self.TABLE_NAME +" values(%s,%s,%s,%s,%s)", (date_to_insert, hour_to_insert, key, audience_total, live_num_total))
+        # date/ hour/ name/ audience_total/ live_num_total
       # load data infile "/Users/xnhuang/Documents/projects/spider/douyustat/douyustat/spiders/2017-06-01csv" into table douyu_tmp fields terminated by ";" enclosed by '"' lines terminated by '\n' ignore 1 rows
+      conn.commit()
+      conn.close()
       return
 
     self.current = self.current + 1;
